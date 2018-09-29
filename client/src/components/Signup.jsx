a89f70427c4dfcb822df9e704f10102a687c42e1
shirley <domo@shirleys-MacBook-Air.local>
2018-09-28 23:26:50 -0400

    Added few skeleton components

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, FormControl, Col, Button, ControlLabel } from 'react-bootstrap'
 
 class Signup extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+
+    }
   }
   render() { 
     return (
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+<Form className="form-panel-signup" horizontal>
+
+  <FormGroup controlId="formHorizontalUsername">
+    <Col componentClass={ControlLabel} sm={4}>
+      Username
+    </Col>
+    <Col sm={3}>
+      <FormControl type="username" placeholder="Username" />
+    </Col>
+  </FormGroup>
+
+  <FormGroup controlId="formHorizontalEmail">
+    <Col componentClass={ControlLabel} sm={4}>
+      Email
+    </Col>
+    <Col sm={3}>
+      <FormControl type="email" placeholder="Email" />
+    </Col>
+  </FormGroup>
+
+  <FormGroup controlId="formHorizontalPassword">
+    <Col componentClass={ControlLabel} sm={4}>
+      Password
+    </Col>
+    <Col sm={3}>
+      <FormControl type="password" placeholder="Password" />
+    </Col>
+  </FormGroup>
+
+  <FormGroup>
+    <Col smOffset={6} sm={3}>
+      <Button type="submit">Create an account</Button>
+    </Col>
+  </FormGroup>
+
+</Form>
+
     )
   }
 }
