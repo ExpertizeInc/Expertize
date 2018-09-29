dae71c0914829edf4eef4f62692ff744a466c23d
Paul Chung <pisc9211@gmail.com>
2018-09-28 22:33:43 -0400

    working on react-router

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Nav, Navbar, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
-import { HashRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import SignIn from './components/SignIn.jsx';
 import Home from './components/Home.jsx';
 import Signup from './components/Signup.jsx';
 import NavBar from './components/NavBar.jsx';
+import Error from './components/Error.jsx';
 
 const Routes = () => (
     <Router>
         <div>
             <NavBar/>
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/signin" component={SignIn}></Route>
-            <Route path="/signup" component={Signup}></Route>
+            <Switch>
+                <Route exact strict path="/" component={Home}></Route>
+                <Route exact strict path="/signin" component={SignIn}></Route>
+                <Route exact strict path="/signup" component={Signup}></Route>
+                <Route exact strict path="/*" component={Error}></Route>
+            </Switch>
         </div>
     </Router>
 )
