d41192717249c5d07f3f433a92b482df00ef868a
Alon Bibring <alon.bibring@gmail.com>
2018-10-07 14:03:20 -0400

    cleaned up some more code and added the ability for us to server our static files from graphql

--- server/index.js
+++ server/index.js
@@ -7,27 +7,25 @@ const cors = require('cors');
 
 var app = express();
 app.use(express.static(path.join(__dirname + '/../client/dist')));
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
 });
+
 var port = process.env.PORT || 3001;
 
-var server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const io = require('socket.io')(server)
 io.on('connect', (socket) => {
-  console.log('a user connected, id is:',socket.id)
-  // socket.on('connectToUser', )s
+  console.log('a user connected, id is:', socket.id);
   socket.on('message', (msg) => {
-    console.log('received message:', msg)
-    io.sockets.emit('outbound', msg)
-  })
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log('received message:', msg);
+    io.sockets.emit('outbound', msg);
   });
+  socket.on('disconnect', () => console.log('user disconnected'));
 });
 
 // const opentok = new OpenTok('46197542', 'b7f3e5f595b2f2e85047e370632074938501d031')
