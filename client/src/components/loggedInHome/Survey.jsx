47732ddff1614031c45356488d9bbacd3816d513
pisc9211 <pisc9211@gmail.com>
2018-10-13 16:44:30 -0400

    fixing merge

--- client/src/components/loggedInHome/Survey.jsx
+++ client/src/components/loggedInHome/Survey.jsx
@@ -11,12 +11,7 @@ class Survey extends Component {
       <div>
         <h1>rate your Expert!</h1>
         <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
+        </form>
       </div>
     )
   }
